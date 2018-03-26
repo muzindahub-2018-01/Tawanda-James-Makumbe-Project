9ed55c4b004c430bb5a37fc6634e516d22b73027
TawasTj <37545213+TawasTj@users.noreply.github.com>
2018-03-26 11:37:57 +0200

    Add files via upload

---
+++ JavaScript/scripts.js
@@ -0,0 +1,16 @@
+
+function regtrack() {
+var fname= prompt (" Enter your Full Name")
+var track = prompt (" Enter Your Track")
+
+If (track.toUpperCase()== FEWD || FRONT END WEB DEVELOPMENT)
+{
+}else{
+Document.write( "Oops! Sorry " + fname + " This is for Front End Web Development not "  + track)
+}
+}
+function promptMe(){
+    var userAdjective = prompt("Please provide an Adjective");
+    alert (userAdjective);
+}
+
